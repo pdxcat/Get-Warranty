bca6eba06ecb9f51302ad6986e8c4218d9d37602
Craig Meinschein <cmeinsch@gmail.com>
2013-03-01 17:42:48 -0800

    Added warranty script.

---
+++ Get-Warranty.ps1
@@ -0,0 +1,61 @@
+#function Get-Warranty {
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true,
+		           ValueFromPipeline=$True,
+				   ValueFromPipelineByPropertyName=$True)]
+		[Alias('Name')]
+		[String[]]$ComputerName,
+		[Parameter(Mandatory=$false)]
+		[Switch]$Latest=$false
+	)
+	$apiKey = '' # Insert Dell API key here - needed to talk to api.dell.com
+	
+	foreach($Computer in $ComputerName) {
+		$computerIsUp = Test-Connection $Computer -Count 1 -Quiet
+		if ($computerIsUp) {
+			# Get Service Tag and Model of target computer.
+			$bios       = Get-WMIobject -Class Win32_BIOS -ComputerName $Computer
+			$system     = Get-WMIobject -Class Win32_ComputerSystem -ComputerName $Computer
+			$serviceTag = $bios | Select-Object -ExpandProperty SerialNumber
+			$compName   = $bios | Select-Object -ExpandProperty __SERVER
+			$model      = $system | Select-Object -ExpandProperty Model
+		
+			# Get warranty information from Dell's website.
+			$wc           = New-Object System.Net.WebClient
+			$url          = "https://api.dell.com/support/v2/assetinfo/warranty/tags?svctags=${serviceTag}&apikey=${apiKey}"
+			[xml]$webData = $wc.DownloadString($url)
+			$warranties   = $webData.getassetwarrantyresponse.getassetwarrantyresult.response.dellasset.warranties.warranty
+		
+			# If the $Latest flag is passed in, filter out all but the most recent warranty.
+			if ($Latest) {
+				$latestWarranty = $warranties[0]
+				foreach ($warranty in $warranties) {	
+					if ((Get-Date $warranty.enddate) -gt (Get-Date $latestWarranty.enddate)) {
+						$latestWarranty = $warranty
+					}
+				}
+				$warranties = $latestWarranty
+			}
+		
+			# Construct and write output object.
+			foreach($warranty in $warranties) {
+				$output = New-Object -Type PSCustomObject
+				Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $compName -InputObject $output
+				Add-Member -MemberType NoteProperty -Name 'Model' -Value $model -InputObject $output
+				Add-Member -MemberType NoteProperty -Name 'ServiceTag' -Value $serviceTag -InputObject $output
+				#Add-Member -MemberType AliasProperty -Name 'Name' -Value 'ComputerName' -InputObject $output
+				# Copy properties from the XML data gotten from Dell.
+				foreach($property in ($warranty | Get-Member -Type Property)) {
+					Add-Member -MemberType NoteProperty -Name $property.name `
+							-Value $warranty.$($property.name) `
+							-InputObject $output
+				}
+			
+				Write-Output $output
+			}
+		} else {
+			Write-Error "$Computer is down."
+		}
+	}
+#}
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 Get-Warranty
 ============
+By: Craig Meinschein (pfaffle)
+With contributions from: Zaynab Alattar (zaalatta)
 
 PowerShell script that retrieves Dell warranty information about a box.
+
+Usage: .\Get-Warranty.ps1 [computername]
+
+This script retrieves the Service Tag from the target computer and then
+queries api.dell.com for warranty information. It outputs it as a custom
+PowerShell object.
+
+The target computer must of course be manufactured by Dell.
+
+The script requires an API Key (string), which you may be able to find
+on the internet, or by talking to your Dell representative.
