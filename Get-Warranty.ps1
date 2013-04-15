491dbf729cb9a3eaa10c147ef5851f2660e7c687
Craig Meinschein <cmeinsch@gmail.com>
2013-04-15 12:14:02 -0700

    Slightly modified script to handle error conditions better.

--- Get-Warranty.ps1
+++ Get-Warranty.ps1
@@ -17,42 +17,47 @@
 			# Get Service Tag and Model of target computer.
 			$bios       = Get-WMIobject -Class Win32_BIOS -ComputerName $Computer
 			$system     = Get-WMIobject -Class Win32_ComputerSystem -ComputerName $Computer
-			$serviceTag = $bios | Select-Object -ExpandProperty SerialNumber
-			$compName   = $bios | Select-Object -ExpandProperty __SERVER
-			$model      = $system | Select-Object -ExpandProperty Model
-		
-			# Get warranty information from Dell's website.
-			$wc           = New-Object System.Net.WebClient
-			$url          = "https://api.dell.com/support/v2/assetinfo/warranty/tags?svctags=${serviceTag}&apikey=${apiKey}"
-			[xml]$webData = $wc.DownloadString($url)
-			$warranties   = $webData.getassetwarrantyresponse.getassetwarrantyresult.response.dellasset.warranties.warranty
-		
-			# If the $Latest flag is passed in, filter out all but the most recent warranty.
-			if ($Latest) {
-				$latestWarranty = $warranties[0]
-				foreach ($warranty in $warranties) {	
-					if ((Get-Date $warranty.enddate) -gt (Get-Date $latestWarranty.enddate)) {
-						$latestWarranty = $warranty
+			$serviceTag = $bios.SerialNumber
+			$compName   = $bios.__SERVER
+			$model      = $system.Model
+			$manuf      = $system.Manufacturer
+			
+			if (!($manuf -match 'Dell')) {
+				Write-Error "Computer not manufactured by Dell. Can't get warranty information."
+			} else {
+				# Get warranty information from Dell's website.
+				$wc           = New-Object System.Net.WebClient
+				$url          = "https://api.dell.com/support/v2/assetinfo/warranty/tags?svctags=${serviceTag}&apikey=${apiKey}"
+				[xml]$webData = $wc.DownloadString($url)
+				$warranties   = $webData.getassetwarrantyresponse.getassetwarrantyresult.response.dellasset.warranties.warranty
+			
+				# If the $Latest flag is passed in, filter out all but the most recent warranty.
+				if ($Latest) {
+					$latestWarranty = $warranties[0]
+					foreach ($warranty in $warranties) {	
+						if ((Get-Date $warranty.enddate) -gt (Get-Date $latestWarranty.enddate)) {
+							$latestWarranty = $warranty
+						}
 					}
-				}
-				$warranties = $latestWarranty
-			}
-		
-			# Construct and write output object.
-			foreach($warranty in $warranties) {
-				$output = New-Object -Type PSCustomObject
-				Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $compName -InputObject $output
-				Add-Member -MemberType NoteProperty -Name 'Model' -Value $model -InputObject $output
-				Add-Member -MemberType NoteProperty -Name 'ServiceTag' -Value $serviceTag -InputObject $output
-				#Add-Member -MemberType AliasProperty -Name 'Name' -Value 'ComputerName' -InputObject $output
-				# Copy properties from the XML data gotten from Dell.
-				foreach($property in ($warranty | Get-Member -Type Property)) {
-					Add-Member -MemberType NoteProperty -Name $property.name `
-							-Value $warranty.$($property.name) `
-							-InputObject $output
+					$warranties = $latestWarranty
 				}
 			
-				Write-Output $output
+				# Construct and write output object.
+				foreach($warranty in $warranties) {
+					$output = New-Object -Type PSCustomObject
+					Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $compName -InputObject $output
+					Add-Member -MemberType NoteProperty -Name 'Model' -Value $model -InputObject $output
+					Add-Member -MemberType NoteProperty -Name 'ServiceTag' -Value $serviceTag -InputObject $output
+					#Add-Member -MemberType AliasProperty -Name 'Name' -Value 'ComputerName' -InputObject $output
+					# Copy properties from the XML data gotten from Dell.
+					foreach($property in ($warranty | Get-Member -Type Property)) {
+						Add-Member -MemberType NoteProperty -Name $property.name `
+								-Value $warranty.$($property.name) `
+								-InputObject $output
+					}
+				
+					Write-Output $output
+				}
 			}
 		} else {
 			Write-Error "$Computer is down."
