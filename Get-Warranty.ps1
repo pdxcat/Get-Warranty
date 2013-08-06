bd021516af2fff30b42e746fa0f4a371daaf6b85
Craig Meinschein <cmeinsch@gmail.com>
2013-08-05 18:29:51 -0700

    Minor edits.

--- Get-Warranty.ps1
+++ Get-Warranty.ps1
@@ -1,66 +1,62 @@
-#function Get-Warranty {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true,
-            ValueFromPipeline=$True,
-            ValueFromPipelineByPropertyName=$True)]
-        [Alias('Name')]
-        [String[]]$ComputerName,
-        [Parameter(Mandatory=$false)]
-        [Switch]$Latest=$false
-    )
-    $apiKey = '1adecee8a60444738f280aad1cd87d0e'
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true,
+               ValueFromPipeline=$True,
+               ValueFromPipelineByPropertyName=$True)]
+    [Alias('Name')]
+    [String[]]$ComputerName,
+    [Parameter(Mandatory=$false)]
+    [Switch]$Latest
+)
+$apiKey = '1adecee8a60444738f280aad1cd87d0e'
 
-    foreach($Computer in $ComputerName) {
-        $computerIsUp = Test-Connection $Computer -Count 1 -Quiet
-        if ($computerIsUp) {
-            # Get Service Tag and Model of target computer.
-            $bios       = Get-WMIobject -Class Win32_BIOS -ComputerName $Computer
-            $system     = Get-WMIobject -Class Win32_ComputerSystem -ComputerName $Computer
-            $serviceTag = $bios.SerialNumber
-            $compName   = $bios.__SERVER
-            $model      = $system.Model
-            $manuf      = $system.Manufacturer
+foreach($Computer in $ComputerName) {
+    $computerIsUp = Test-Connection $Computer -Count 1 -Quiet
+    if (!$computerIsUp) {
+        Write-Error "$Computer is down."
+    } else {
+        # Get Service Tag and Model of target computer.
+        $bios       = Get-WMIobject -Class Win32_BIOS -ComputerName $Computer
+        $system     = Get-WMIobject -Class Win32_ComputerSystem -ComputerName $Computer
+        $serviceTag = $bios.SerialNumber
+        $compName   = $bios.__SERVER
+        $model      = $system.Model
+        $manuf      = $system.Manufacturer
 
-            if (!($manuf -match 'Dell')) {
-                Write-Error "Computer not manufactured by Dell. Can't get warranty information."
-            } else {
-                # Get warranty information from Dell's website.
-                $wc           = New-Object System.Net.WebClient
-                $url          = "https://api.dell.com/support/v2/assetinfo/warranty/tags?svctags=${serviceTag}&apikey=${apiKey}"
-                [xml]$webData = $wc.DownloadString($url)
-                $warranties   = $webData.getassetwarrantyresponse.getassetwarrantyresult.response.dellasset.warranties.warranty
-
-                # If the $Latest flag is passed in, filter out all but the most recent warranty.
-                if ($Latest) {
-                    $latestWarranty = $warranties[0]
-                    foreach ($warranty in $warranties) {	
-                        if ((Get-Date $warranty.enddate) -gt (Get-Date $latestWarranty.enddate)) {
-                            $latestWarranty = $warranty
-                        }
-                    }
-                    $warranties = $latestWarranty
-                }
-
-                # Construct and write output object.
-                foreach($warranty in $warranties) {
-                    $output = New-Object -Type PSCustomObject
-                    Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $compName -InputObject $output
-                    Add-Member -MemberType NoteProperty -Name 'Model' -Value $model -InputObject $output
-                    Add-Member -MemberType NoteProperty -Name 'ServiceTag' -Value $serviceTag -InputObject $output
-                    #Add-Member -MemberType AliasProperty -Name 'Name' -Value 'ComputerName' -InputObject $output
-                    # Copy properties from the XML data gotten from Dell.
-                    foreach($property in ($warranty | Get-Member -Type Property)) {
-                        Add-Member -MemberType NoteProperty -Name $property.name `
-                            -Value $warranty.$($property.name) `
-                            -InputObject $output
-                    }
-
-                Write-Output $output
-                }
-            }
+        if (!($manuf -match 'Dell')) {
+            Write-Error "Computer not manufactured by Dell. Can't get warranty information."
         } else {
-            Write-Error "$Computer is down."
+            # Get warranty information from Dell's website.
+            $wc           = New-Object System.Net.WebClient
+            $url          = "https://api.dell.com/support/v2/assetinfo/warranty/tags?svctags=${serviceTag}&apikey=${apiKey}"
+            [xml]$webData = $wc.DownloadString($url)
+            $warranties   = $webData.getassetwarrantyresponse.getassetwarrantyresult.response.dellasset.warranties.warranty
+
+            # If the $Latest paramater is given, filter out all but the most recent warranty.
+            if ($Latest) {
+                $latestWarranty = $warranties[0]
+                foreach ($warranty in $warranties) {	
+                    if ((Get-Date $warranty.enddate) -gt (Get-Date $latestWarranty.enddate)) {
+                        $latestWarranty = $warranty
+                    }
+                }
+                $warranties = $latestWarranty
+            }
+
+            # Construct and write output object.
+            foreach($warranty in $warranties) {
+                $output = New-Object -Type PSCustomObject
+                Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $compName -InputObject $output
+                Add-Member -MemberType NoteProperty -Name 'Model' -Value $model -InputObject $output
+                Add-Member -MemberType NoteProperty -Name 'ServiceTag' -Value $serviceTag -InputObject $output
+                # Copy properties from the XML data gotten from Dell.
+                foreach($property in ($warranty | Get-Member -Type Property)) {
+                    Add-Member -MemberType NoteProperty -Name $property.name `
+                        -Value $warranty.$($property.name) `
+                        -InputObject $output
+                }
+                Write-Output $output
+            }
         }
     }
-#}
+}
